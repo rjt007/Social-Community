aeb77d7e8a8931ea8982ae6e891f759d11b143de
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-14 17:41:15 +0530

    create models/user.js to create userSchema

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    id:{
+        type: String,
+        unique: true,
+        required:true
+    },
+    name:{
+        type: String,
+        maxLength: 64,
+        default: null
+    },
+    email:{
+        type: String,
+        maxLength: 128,
+        unique: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        maxLength: 64,
+        required: true
+    },
+    created_at:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
