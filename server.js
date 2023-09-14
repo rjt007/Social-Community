35c561743e7f0cd66763f7557c672caeb401f0e4
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-14 23:17:05 +0530

    install @theinternetfolks/snowflake to generate random strings to use it as id

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const roleRoutes = require('./routes/roles');
+
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.json({request:'success'});
 });
+
+app.use('/v1/role',roleRoutes);
 
 //Connecting to database
 const connectDB = require('./config/db');
