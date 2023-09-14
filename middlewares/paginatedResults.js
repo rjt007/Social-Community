f1456bdfb6c2ebacbe50272d6c18095fd42f16b4
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-14 23:11:47 +0530

    create middlewares/paginatedResults to send paginated response to the user

---
+++ middlewares/paginatedResults.js
@@ -0,0 +1,33 @@
+
+const paginatedResults = (model) => async(req, res, next) => {
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const total = await model.countDocuments().exec();
+    const pages = total/limit;
+
+    const startIndex = (page - 1) * limit;
+    //const endIndex = page * limit;
+
+    try {
+      const data = await model.find({},{_id: 0, __v: 0}).limit(limit).skip(startIndex).exec();
+      const results = {
+        status: true,
+        content: {
+            meta: {
+                total: total,
+                pages: pages,
+                page: page
+            }
+        },
+        data: data
+    };
+      res.results = results;
+      next();
+    }
+    catch (err) {
+      res.status(500).json({message: err.message});
+    }
+};
+
+module.exports = paginatedResults;
