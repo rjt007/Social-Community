8eddcc79f2aeefa1c34c6ea379a8da343b6c2340
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-09-17 17:02:50 +0530

    add cors setting to allow cross-origin requests

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const rolesRoute = require('./routes/roles');
 const usersRoute = require('./routes/users');
 const communitiesRoute = require('./routes/communities');
 const membersRoute = require('./routes/members');
 
+//CORS Setting
+const CorsOptions = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+      'PUT',
+      'DELETE',
+      'PATCH'
+    ],
+  
+    allowedHeaders: [
+      'Content-Type', 'Authorization'
+    ],
+};
+  
+app.use(cors(CorsOptions));
 app.use(express.json());
 
 // Registering different routes
